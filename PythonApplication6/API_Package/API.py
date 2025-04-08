35e19798bde3204e0b3eb9306c22d3cbb9a0c028
JPowell2003 <75809385+JPowell2003@users.noreply.github.com>
2025-04-08 13:18:01 -0400

    Add APIClient class to retrieve Pokemon data from PokeAPI

--- PythonApplication6/API_Package/API.py
+++ PythonApplication6/API_Package/API.py
@@ -1,1 +1,35 @@
+# File Name : API.py
+# Student Name: Jay Powell
+# email: powela9@mail.uc.edu
+# Assignment Number: Assignment 10 
+# Due Date: 4/10/2025
+# Course #/Section:   IS4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:
+# Brief Description of what this module does:
+    #Connects to the PokeAPI to retrieve data about the Pokémon Ditto.
+# Citations: https://pokeapi.co/
+# Anything else that's relevant:
+
+import requests
+import json
+
+class APIClient:
+    '''
+    Connects to the PokeAPI and retrieves information about a specific Pokémon.
+    '''
+    def __init__(self):
+        '''
+        Initializes the APIClient with a URL pointing to the Ditto Pokémon
+        '''
+        self.url = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+    def get_pokemon(self):
+        '''
+        Sends a get request to the PokeAPI and retrieves the data for Ditto.
+        @return: A dictionary containing data about the Pokémon Ditto.
+        '''
+        response = requests.get(self.url)
+        data = json.loads(response.content)
+        return data
 
