00982fd2fc1049b1a18ec4a36520f66b0dcd691f
Shantele King <Shantele.kingsch@gmail.com>
2025-04-10 11:52:26 -0400

    Small CHANGES

--- PythonApplication6/main_Package/main.py
+++ PythonApplication6/main_Package/main.py
@@ -6,7 +6,9 @@
 # Course #/Section: IS4010-001
 # Semester/Year:Spring 2025
 # Brief Description of the assignment:
-# Brief Description of what this module does: Calls the class from the 
+    # The assignment gets data about the Pokémon Ditto from an API, processes it into a CSV file, and displays some of its basic information
+# Brief Description of what this module does:
+    # Calls the class from the API and DataProcessor files to get Ditto’s data, save it to a CSV, and print some of its details
 # Citations: https://pokeapi.co/
 # Anything else that's relevant:
 
@@ -22,9 +24,6 @@ if __name__ == "__main__":
     Data_Processor = DataProcessor(ditto_data)
     Data_Processor.save_to_csv("ditto_data.csv")
 
-    
-
-    
     name = ditto_data.get("name")
     height = ditto_data.get("height")
     weight = ditto_data.get("weight")
@@ -34,6 +33,7 @@ if __name__ == "__main__":
     print("Height:", height, "dm")
     print("Weight:", weight, "hg")
     print("Abilities:", ", ".join(abilities))
+<<<<<<< HEAD
 
 
 
@@ -46,3 +46,5 @@ if __name__ == "__main__":
 
 
 
+=======
+>>>>>>> 955c1b6cde6d00ff08f5cd141adcd781cb660851
